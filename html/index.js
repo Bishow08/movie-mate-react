ec84aa94bd3d1398624a6dbea59c886ce947c52c
Bishow08 <gdghamal522@gmail.com>
2024-05-30 14:30:10 +0545

    form css added with formdata handling

--- html/index.js
+++ html/index.js
@@ -7,4 +7,11 @@ const formSubmission = ()=>{
     for (const value of formDatas.values()) {
         console.log(value);
     }
+    document.getElementById("contactForm").reset();
+    document.getElementsByClassName("systemMsg")[0].innerHTML = "Thank you for contacting us";
+
+    setTimeout(()=>{
+        document.getElementsByClassName("systemMsg")[0].innerHTML = "";
+    }, 3000);
+
 }
