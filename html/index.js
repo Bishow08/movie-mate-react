37c21965d37bd6c04850fe1c073ef70d35e7ab1d
Bishow08 <gdghamal522@gmail.com>
2024-05-31 17:50:35 +0545

    js revised

--- html/index.js
+++ html/index.js
@@ -22,3 +22,51 @@ const str = "1234567890"
 
     //ternary operator
     //condition ? true : false;
+
+    const checkEvenOdd = (a) => {
+        return a % 2 === 0 ? console.log("even") : console.log("odd");
+    }
+     checkEvenOdd(50);
+
+    //  const properCase = (str) => {
+    //     return str.split(" ")
+    //     .map((word)=> word.charAt(0).toUpperCase)
+    //  }
+    // console.log(properCase());
+
+    const nepalNumber = (number) => { 
+        return number.length === 10 &&
+            (number.slice(0, 2) === "98" || number.slice(0, 2) === "97")? true : false;
+    }
+    
+    console.log(nepalNumber("9812345678")); // false
+
+    // Default Function
+
+    // Parametrized Function (parameters wrap in curly braces)
+    const add = ({a = 2, b=3, c= 5}) => {
+        return a + b + c;
+    }
+    //while calling should pass object as {}
+    console.log(add({}));
+
+    // Explicit Function
+    const sayHello = (name) => {
+        return `Hi $(name)`;
+    }
+    console.log(sayHello("Bishal"));
+
+    // Implicit Function
+    const Hello = (name) => 
+         `Hi $(name)`;
+    
+    console.log(Hello("Bishal"));
+
+    // IIFE (Immediately Invoked Function Expressions)
+    // used in Project setup js
+    // also Devops (script immediately run)
+
+    (()=>{
+        console.log("Immediately run")
+    })();
+    
